4975fe1a1076121fbe12295cb2ed45e95968a3cd
Julian Gallo <julian.gallo2@nexergroup.com>
2026-05-27 05:46:54 -0500

    Add project files.

---
+++ Singleton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    public class Singleton
+    {
+        private static Singleton _instance;
+
+        private Singleton()
+        {
+            
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Singleton();
+                }
+                return _instance;
+            }
+        }
+    }
+}
